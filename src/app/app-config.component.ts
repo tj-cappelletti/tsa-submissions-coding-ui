ea85f9d3e6c747b595f9d79f9399c34226ebec0b
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2022-04-20 08:02:17 -0400

    Began integration of Monaco Editor
    Switched styles to SCSS to help with visualizations
    Created editor module based on ngx-monaco-editor and updated as needed
    Added editor to the challenge component
    Still a work in progress

--- src/app/app-config.component.ts
+++ src/app/app-config.component.ts
@@ -1,9 +1,9 @@
 export class AppConfig {
   //TODO: Move to API configuration
-  static _apiRoot = 'http://api.tsa.local/api/';
+  static _apiRoot = 'https://api.tsa.local:44300/api/';
   static _clientId = 'tsa.submissions.coding.web';
   static _clientRoot = 'http://localhost:4200/';
-  static _stsAuthority = 'http://localhost:5001/';
+  static _stsAuthority = 'https://identity.tsa.local:44301/';
 
   static apiRoot():string {
     return this._apiRoot;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,33 @@
 import {Component} from '@angular/core';
-import {AuthService} from './services/auth.service';
+import {AuthService} from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   isLoggedIn: Boolean = false;
-  title = 'tsa-submissions-coding-ui';
   year = new Date().getFullYear();
 
   constructor(private _authService: AuthService) {
-    this._authService.loginChanged.subscribe(loggedIn => {
+    this._authService.loginChanged.subscribe((loggedIn: Boolean) => {
       this.isLoggedIn = loggedIn;
     });
   }
 
   ngOnInit() {
-    this._authService.isLoggedIn().then(loggedIn => {
+    this._authService.isLoggedIn().then((loggedIn: Boolean) => {
       this.isLoggedIn = loggedIn;
     });
+  }
+  
+  login() {
+    this._authService.login();
+  }
+
+  logout() {
+    this._authService.logout();
   }
 }
