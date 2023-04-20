f9de57bcb0469ae6abb7846eb43aff4276d71f3e
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2023-04-20 18:19:00 -0400

    Added Blazor UI

---
+++ src/Tsa.Submissions.Coding.Ui.Blazor/Models/ProblemModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Tsa.Submissions.Coding.Ui.Blazor.Models
+{
+    public class ProblemModel
+    {
+        public string? Description { get; set; }
+
+        public string? Id { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string? Title { get; set; }
+    }
+}
