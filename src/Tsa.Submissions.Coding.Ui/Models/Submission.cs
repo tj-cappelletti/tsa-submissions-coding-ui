0df2d9e97976318a37992fdbd8331fd52f9a53f6
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-04-23 08:11:12 -0400

    Added ability to view submissions

---
+++ src/Tsa.Submissions.Coding.Ui/Models/Submission.cs
@@ -0,0 +1,43 @@
+﻿namespace Tsa.Submissions.Coding.Ui.Models;
+
+public class Submission
+{
+    public string? Id { get; set; }
+
+    public bool IsFinalSubmission { get; set; }
+
+    public string? Language { get; set; }
+
+    public string? ProblemId { get; set; }
+
+    public string? Solution { get; set; }
+
+    public DateTime? SubmittedOn { get; set; }
+
+    public string? TeamId { get; set; }
+
+    public IList<TestSetResult>? TestSetResults { get; set; }
+
+    public string? Result
+    {
+        get
+        {
+            if (TestSetResults == null || TestSetResults.Count == 0)
+            {
+                return "Pending";
+            }
+
+            if (TestSetResults.All(x => x.Passed))
+            {
+                return "Passed";
+            }
+
+            if (TestSetResults.Any(x => x.Passed))
+            {
+                return "Partially Passed";
+            }
+
+            return "Failed";
+        }
+    }
+}
---
+++ src/Tsa.Submissions.Coding.Ui/Models/TestSetResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Tsa.Submissions.Coding.Ui.Models;
+
+public class TestSetResult
+{
+    public bool Passed { get; set; }
+
+    public TimeSpan? RunDuration { get; set; }
+
+    public string? TestSetId { get; set; }
+}
