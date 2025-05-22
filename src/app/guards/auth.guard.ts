8d04663c30739a3d835c0960c59ca1de6bfd538c
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-05-22 07:08:49 -0400

    Refactored authGuard to follow best practices

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -9,7 +9,6 @@ export const authGuard: CanActivateFn = () => {
   if (auth.isAuthenticated()) {
     return true;
   } else {
-    router.navigate(['/login']);
-    return false;
+    return router.createUrlTree(['/login']);
   }
 };
