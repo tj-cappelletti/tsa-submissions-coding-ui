b24708feb40c8e3d4313ec2f10be28a66426435c
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-05-22 07:07:01 -0400

    Refactored AuthService
    Improved logic for validating token
    Simplified how accessors get data from token

--- src/app/models/auth.models.ts
+++ src/app/models/auth.models.ts
@@ -3,6 +3,13 @@ export interface Authentication {
     userName: string;
 }
 
+export interface JwtPayload {
+  exp: number;
+  iat: number;
+  role: string;
+  unique_name: string;
+}
+
 export interface LoginResponse {
   expiration: string;
   token: string;
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
 import { of, Observable } from 'rxjs';
-import { Authentication, LoginResponse } from '../models/auth.models';
+import { Authentication, JwtPayload, LoginResponse } from '../models/auth.models';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
@@ -14,55 +14,60 @@ export class AuthService {
   }
 
   getUserName(): string | null {
-    const token = this.getToken();
+    const jwtPayload = this.getTokenPayload();
 
-    try {
-      if (token) {
-        const payload = JSON.parse(atob(token.split('.')[1]));
+    if (jwtPayload === null) return null;
 
-        return payload.unique_name;
-      }
-      return null;
-    } catch (error) {
-      console.error('Error parsing token:', error);
-
-      return null;
-    }
+    return jwtPayload.unique_name;
   }
 
   getUserRole(): string | null {
-    const token = this.getToken();
-    
-    try {
-      if (token) {
-        const payload = JSON.parse(atob(token.split('.')[1]));
+    const jwtPayload = this.getTokenPayload();
 
-        return payload.role;
-      }
+    if (jwtPayload === null) return null;
+
+    return jwtPayload.role;
+  }
+
+  private getTokenPayload(): JwtPayload | null {
+    const token = localStorage.getItem(this.jwtKeyName);
+
+    if (!token) {
+      console.log('No token found in local storage');
       return null;
+    }
+
+    try {
+      const payload = JSON.parse(atob(token.split('.')[1])) as JwtPayload;
+
+      // Check if the payload is valid
+      if (!payload) {
+        console.log('Invalid token payload');
+        return null;
+      }
+
+      if (!payload.exp || !payload.iat || !payload.role || !payload.unique_name) {
+        console.log('Token payload is missing required fields');
+        return null;
+      }
+
+      return payload;
     } catch (error) {
       console.error('Error parsing token:', error);
-      
       return null;
     }
   }
 
-  getToken(): string | null {
-    return localStorage.getItem(this.jwtKeyName);
-  }
-
   isAuthenticated(): boolean {
-    const token = localStorage.getItem(this.jwtKeyName);
+    const jwtPayload = this.getTokenPayload();
 
-    if (!token) return false;
+    if (jwtPayload === null) return false;
 
     try {
-      const payload = JSON.parse(atob(token.split('.')[1]));
-
       // JWT exp is in seconds since epoch
       const now = Math.floor(Date.now() / 1000);
 
-      if (payload.exp && payload.exp < now) {
+      if (jwtPayload.exp < now) {
         // Token expired
         return false;
       }
@@ -76,7 +81,7 @@ export class AuthService {
   }
 
   login(username: string, password: string): Observable<boolean> {
-    var authenticationModel: Authentication = {
+    const authenticationModel: Authentication = {
       password: password,
       userName: username
     };
