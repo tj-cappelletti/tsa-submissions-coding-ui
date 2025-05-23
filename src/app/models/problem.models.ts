ab60447b1b1bb1f925ff9a8485aebafbf5a72f1a
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-05-23 15:46:37 -0400

    Updated problems model to work with HTML forms

--- src/app/models/problem.models.ts
+++ src/app/models/problem.models.ts
@@ -1,21 +1,21 @@
 export interface Problem {
-    description:string;
-    id:string;
-    isActive:boolean;
-    testSets:TestSetModel[];
-    title:string;
+    description: string;
+    id?: string | null | undefined;
+    isActive?: boolean | null | undefined;
+    title: string;
+    testSets?: TestSetModel[] | null | undefined;
 }
 
 export interface TestSetModel {
-    id:string;
-    inputs:TestSetValueModel[];
-    isPublic:boolean;
-    name:string;
-    problemId:string;
+    id: string;
+    inputs: TestSetValueModel[];
+    isPublic: boolean;
+    name: string;
+    problemId: string;
 }
 
 export interface TestSetValueModel {
-    id:string;
+    id: string;
     dataType: string;
     index: number;
     isArray: boolean;
