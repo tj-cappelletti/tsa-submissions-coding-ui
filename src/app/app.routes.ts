1eda24254b07e0dc58de3313d0275c112bdfca3b
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-05-23 15:47:29 -0400

    Added routes viewing, creating, and editing problems

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ProblemsComponent } from './components/problems/problems.component';
+import { ProblemFormComponent } from './components/problems/problem-form.component';
 import { authGuard } from './guards/auth.guard';
+import { judgeRoleGuard } from './guards/judge-role.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'problems', component: ProblemsComponent, canActivate: [authGuard] },
+  { path: 'problems/:id/edit', component: ProblemFormComponent, canActivate: [judgeRoleGuard] },
+  { path: 'problems/new', component: ProblemFormComponent, canActivate: [judgeRoleGuard] },
 ];
