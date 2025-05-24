1f82cc50f6e74f3cba4c2b1257fcb91e69c5c87e
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-05-24 19:53:22 -0400

    Fixed route priority

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ProblemComponent } from './components/problems/problem.component';
 import { ProblemsComponent } from './components/problems/problems.component';
 import { ProblemFormComponent } from './components/problems/problem-form.component';
 import { authGuard } from './guards/auth.guard';
@@ -12,6 +13,7 @@ export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'problems', component: ProblemsComponent, canActivate: [authGuard] },
-  { path: 'problems/:id/edit', component: ProblemFormComponent, canActivate: [judgeRoleGuard] },
   { path: 'problems/new', component: ProblemFormComponent, canActivate: [judgeRoleGuard] },
+  { path: 'problems/:id/edit', component: ProblemFormComponent, canActivate: [judgeRoleGuard] },
+  { path: 'problems/:id', component: ProblemComponent, canActivate: [authGuard] },
 ];
