904708796086381440cfe3801d63a395ea6c9d25
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-06-07 12:16:28 -0400

    Configured Monaco editory to work with project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideMarkdown } from 'ngx-markdown';
 
 import { routes } from './app.routes';
+import { MonacoEditorModule, NgxMonacoEditorConfig } from 'ngx-monaco-editor-v2';
+
+export const monacoConfig: NgxMonacoEditorConfig = {
+  baseUrl: window.location.origin + "/assets/monaco/min/vs"
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(MonacoEditorModule.forRoot(monacoConfig)),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
